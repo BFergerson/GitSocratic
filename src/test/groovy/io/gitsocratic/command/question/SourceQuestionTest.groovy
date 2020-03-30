407aee3ea8c4f31397e4d1b07cef2995204750df
Brandon Fergerson <fergerson92@gmail.com>
2020-03-29 21:11:46 -0400

    use literals instead of method names

--- src/test/groovy/io/gitsocratic/command/question/SourceQuestionTest.groovy
+++ src/test/groovy/io/gitsocratic/command/question/SourceQuestionTest.groovy
@@ -1,68 +1,71 @@
 package io.gitsocratic.command.question
 
-import org.codehaus.groovy.runtime.StackTraceUtils
 import org.junit.Test
 
-import static org.junit.Assert.*
+import static org.junit.Assert.assertEquals
 
 class SourceQuestionTest {
 
     @Test
-    void "how many [language] methods are named [name]"() {
+    void howManyXMethodsAreNamedX() {
+        def questionLiteral = "how many [language] methods are named [name]"
         def question = SourceQuestion.toSourceQuestion("how many java methods are named main")
-        assertEquals(currentMethodName, question.formattedQuestion)
+        assertEquals(questionLiteral, question.formattedQuestion)
     }
 
     @Test
-    void "how many [language] methods total"() {
+    void howManyXMethodsTotal() {
+        def questionLiteral = "how many [language] methods total"
         def question = SourceQuestion.toSourceQuestion("how many go methods total")
-        assertEquals(currentMethodName, question.formattedQuestion)
+        assertEquals(questionLiteral, question.formattedQuestion)
     }
 
     @Test
-    void "how many methods are named [name]"() {
+    void howManyMethodsAreNamedX() {
+        def questionLiteral = "how many methods are named [name]"
         def question = SourceQuestion.toSourceQuestion("how many methods are named main")
-        assertEquals(currentMethodName, question.formattedQuestion)
+        assertEquals(questionLiteral, question.formattedQuestion)
     }
 
     @Test
-    void "how many methods are named like [name]"() {
+    void howManyMethodsAreNamedLikeX() {
+        def questionLiteral = "how many methods are named like [name]"
         def question = SourceQuestion.toSourceQuestion("how many methods are named like main")
-        assertEquals(currentMethodName, question.formattedQuestion)
+        assertEquals(questionLiteral, question.formattedQuestion)
     }
 
     @Test
-    void "how many methods total"() {
+    void howManyMethodsTotal() {
+        def questionLiteral = "how many methods total"
         def question = SourceQuestion.toSourceQuestion("how many methods total")
-        assertEquals(currentMethodName, question.formattedQuestion)
+        assertEquals(questionLiteral, question.formattedQuestion)
     }
 
     @Test
-    void "what are the [limit] most complex [language] methods"() {
+    void whatAreTheXMostComplexXMethods() {
+        def questionLiteral = "what are the [limit] most complex [language] methods"
         def question = SourceQuestion.toSourceQuestion("what are the 5 most complex ruby methods")
-        assertEquals(currentMethodName, question.formattedQuestion)
+        assertEquals(questionLiteral, question.formattedQuestion)
     }
 
     @Test
-    void "what are the [limit] most complex methods"() {
+    void whatAreTheXMostComplexMethods() {
+        def questionLiteral = "what are the [limit] most complex methods"
         def question = SourceQuestion.toSourceQuestion("what are the 10 most complex methods")
-        assertEquals(currentMethodName, question.formattedQuestion)
+        assertEquals(questionLiteral, question.formattedQuestion)
     }
 
     @Test
-    void "what is the most complex [language] method"() {
+    void whatIsTheMostComplexXMethod() {
+        def questionLiteral = "what is the most complex [language] method"
         def question = SourceQuestion.toSourceQuestion("what is the most complex python method")
-        assertEquals(currentMethodName, question.formattedQuestion)
+        assertEquals(questionLiteral, question.formattedQuestion)
     }
 
     @Test
-    void "what is the most complex method"() {
+    void whatIsTheMostComplexMethod() {
+        def questionLiteral = "what is the most complex method"
         def question = SourceQuestion.toSourceQuestion("what is the most complex method")
-        assertEquals(currentMethodName, question.formattedQuestion)
-    }
-
-    private static String getCurrentMethodName() {
-        def marker = new Throwable()
-        return StackTraceUtils.sanitize(marker).stackTrace[1].methodName
+        assertEquals(questionLiteral, question.formattedQuestion)
     }
 }
