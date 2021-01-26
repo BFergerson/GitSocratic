ad1d0798a13793221177986e33d5b0ed2ba96c9d
Brandon Fergerson <bfergerson@apache.org>
2021-01-26 18:54:11 -0500

    version bump

--- src/main/groovy/io/gitsocratic/command/config/ImportMode.groovy
+++ src/main/groovy/io/gitsocratic/command/config/ImportMode.groovy
@@ -1,7 +1,7 @@
 package io.gitsocratic.command.config
 
 /**
- * @since 0.2.2
+ * @since 0.3.0
  * @author <a href="mailto:brandon.fergerson@codebrig.com">Brandon Fergerson</a>
  */
 enum ImportMode {
